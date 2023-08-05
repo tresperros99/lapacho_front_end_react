0de418ffc3e1afc6c014e03928d03fc262b16c92
Lucas Torres <63941217+tresperros99@users.noreply.github.com>
2023-08-05 11:53:28 -0400

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import './App.css'
+import { RootState } from './app/store'
+import reactLogo from './assets/react.svg'
+import { increment } from './features/counter/counterSlice'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const {value} = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch()
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button  onClick={() => dispatch(increment())}>
+          count is {value}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
