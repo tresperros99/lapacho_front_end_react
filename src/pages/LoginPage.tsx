190778d9fa2467a448ef840ed01f33d2235215c2
Lucas Torres <63941217+tresperros99@users.noreply.github.com>
2023-08-28 17:50:44 -0400

    Modificaciones en el Login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -10,38 +10,24 @@ import Paper from '@mui/material/Paper';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
-import { useEffect } from 'react';
-import lapachoLogo from '../assets/login/lapacho_logo.jpg';
 import { useAppDispatch } from '../app/store';
+import lapachoLogo from '../assets/login/lapacho_logo.jpg';
 import { fetchLogin } from '../features/auth/authThunk';
-function Copyright(props: any) {
-  return (
-	<Typography variant="body2" color="text.secondary" align="center" {...props}>
-	  {'Copyright © '}
-	  <Link color="inherit" href="https://mui.com/">
-		Proyecto Lapacho
-	  </Link>{' '}
-	  {new Date().getFullYear()}
-	  {'.'}
-	</Typography>
-  );
-}
+
 
 // TODO remove, this demo shouldn't need to reset the theme.
 export const LoginPage = () => {
-	const dispatch = useAppDispatch()
-	useEffect(() => {
-		dispatch(fetchLogin({usuario:'ADMINISTRADOR_CLUB',contraseña:'Prueb4_X_4dm1n'}))
-	}, [dispatch])
+	const dispatch = useAppDispatch();
 	
-
 	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
 		const data = new FormData(event.currentTarget);
-		console.log({
-		email: data.get('email'),
-		password: data.get('password'),
-		});
+		const usuario = data.get('email');
+		const pass = data.get('password');
+		if (usuario && pass) {
+			dispatch(fetchLogin({usuario:usuario.toString(),contraseña:pass.toString()}))
+		}
+
 	};
 
   return (
@@ -61,71 +47,78 @@ export const LoginPage = () => {
 		  }}
 		/>
 		<Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-		  <Box
-			sx={{
-			  my: 8,
-			  mx: 4,
-			  display: 'flex',
-			  flexDirection: 'column',
-			  alignItems: 'center',
-			}}
-		  >
-			<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-			  <LockOutlinedIcon />
-			</Avatar>
-			<Typography component="h1" variant="h5">
-			  Iniciar Sesion
-			</Typography>
-			<Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
-			  <TextField
-				margin="normal"
-				required
-				fullWidth
-				id="email"
-				label="Email"
-				name="email"
-				autoComplete="email"
-				autoFocus
-			  />
-			  <TextField
-				margin="normal"
-				required
-				fullWidth
-				name="password"
-				label="Contraseña"
-				type="password"
-				id="password"
-				autoComplete="current-password"
-			  />
-			  <FormControlLabel
-				control={<Checkbox value="remember" color="primary" />}
-				label="Recordarme"
-			  />
-			  <Button
-				type="submit"
-				fullWidth
-				variant="contained"
-				sx={{ mt: 3, mb: 2 }}
-			  >
-				Iniciar Sesion
-			  </Button>
-			  <Grid container>
-				<Grid item xs>
-				  <Link href="#" variant="body2">
-					Olvidaste la Contraseña
-				  </Link>
+			<Box
+			sx={{my: 8,mx: 4,display: 'flex',flexDirection: 'column',alignItems: 'center',}}
+			>
+				<Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+				<LockOutlinedIcon />
+				</Avatar>
+				<Typography component="h1" variant="h5">
+					Iniciar sesión
+				</Typography>
+				<Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						id="email"
+						label="Email"
+						name="email"
+						autoComplete="email"
+						autoFocus
+					/>
+					<TextField
+						margin="normal"
+						required
+						fullWidth
+						name="password"
+						label="Contraseña"
+						type="password"
+						id="password"
+						autoComplete="current-password"
+					/>
+					<FormControlLabel
+						control={<Checkbox value="remember" color="primary" />}
+						label="Recordarme"
+					/>
+					<Button
+						type="submit"
+						fullWidth
+						variant="contained"
+						sx={{ mt: 3, mb: 2 }}
+					>
+					Iniciar Sesion
+				</Button>
+				<Grid container>
+					<Grid item xs>
+						<Link href="#" variant="body2">
+							Olvidaste la Contraseña
+						</Link>
+					</Grid>
+					<Grid item>
+						<Link href="#" variant="body2">
+							{"Create una!!"}
+						</Link>
+					</Grid>
 				</Grid>
-				<Grid item>
-				  <Link href="#" variant="body2">
-					{"Create una!!"}
-				  </Link>
-				</Grid>
-			  </Grid>
-			  <Copyright sx={{ mt: 5 }} />
+				<Copyright sx={{ mt: 5 }} />
+				</Box>
 			</Box>
-		  </Box>
 		</Grid>
 	</Grid>
   );
 }
 export default LoginPage;
+
+function Copyright(props: any) {
+	return (
+	  <Typography variant="body2" color="text.secondary" align="center" {...props}>
+		{'Copyright © '}
+		<Link color="inherit" href="https://mui.com/">
+		  Proyecto Lapacho
+		</Link>{' '}
+		{new Date().getFullYear()}
+		{'.'}
+	  </Typography>
+	);
+  }
